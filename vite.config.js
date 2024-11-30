4d751841685c20dc7cedaf0f77c7baeccc022ea9
Altzaza <achirayu19@gmail.com>
2024-12-01 05:35:48 +0700

    3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/_slideme_admin/'
+  // base: '/_slideme_admin/'
 })
